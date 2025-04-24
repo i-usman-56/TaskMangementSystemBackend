a38a2e0b171c8bf3cd4eae09df2f0b63f0747191
Muhammad Usman <ranausman417335@gmail.com>
2025-04-24 23:50:12 +0500

    Minor Changes

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 const ConnectDataBase = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to " + process.env.MONGO_URL)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected to : ${process.env.MONGO_URL}`.red.underline
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default ConnectDataBase;
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 import express from "express";
 import cors from "cors";
-import ConnectDataBase from './config/connection.js';  // Import the async function to connect to MongoDB
-import authRoutes from "./routes/authRoutes.js"
-import taskRoutes from "./routes/taskRoutes.js"
-import favoritesRoutes from "./routes/favoriteRoutes.js"
-import dotenv from 'dotenv';
+import colors from "colors";
+import ConnectDataBase from "./config/connection.js"; // Import the async function to connect to MongoDB
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+import favoritesRoutes from "./routes/favorite.routes.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 5050;
-const app = express();  
-
-// Cors policy 
+const app = express();
+colors.enable();
+// Cors policy
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use('/api/favorites', favoritesRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/favorites", favoritesRoutes);
 // Root route to verify server is running
-app.use('/', (req, res) => {
+app.use("/", (req, res) => {
   res.send(`Server is Running ${process.env.PORT} `);
 });
 // Routes
 
 // Connect to the database and start the server
 app.listen(PORT, async () => {
-   ConnectDataBase();  // Ensure database connection is established
-  console.log(`Server listening on port ${PORT}`);
+  ConnectDataBase(); // Ensure database connection is established
+  console.log(`Server listening on port ${PORT}`.blue.underline);
 });
